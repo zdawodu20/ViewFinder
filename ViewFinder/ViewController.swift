bbeb7bd49bd921a213955883ed73c9d4fbd11b11
Apple <apple@Apples-MacBook-Pro-67.local>
2019-06-24 13:32:32 -0400

    Initial Commit

---
+++ ViewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
